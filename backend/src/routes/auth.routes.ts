722a473be52726be3be2507f061bda34209af9b1
Sagar Kadel <sagarkadel97@gmail.com>
2026-01-05 02:33:06 +0545

    Add routes and types

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/user.controller";
+
+let authController = new AuthController();
+const router = Router();
+
+router.post("/register", authController.register)
+router.post("/login", authController.login)
+// add remaining routes like login, logout, etc.
+
+export default router;
