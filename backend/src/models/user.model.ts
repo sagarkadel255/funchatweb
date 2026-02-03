bedbf0a4df7d03dad052b9e6aa647ee703f94f7c
Sagar Kadel <sagarkadel97@gmail.com>
2026-02-03 19:56:37 +0545

    Update user model for Sprint 3

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,16 +1,41 @@
 import mongoose, { Document, Schema } from "mongoose";
 import { UserType } from "../types/user.type";
+
 const UserSchema: Schema = new Schema<UserType>(
     {
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        username: { type: String, required: true, unique: true },
-        firstName: { type: String },
-        lastName: { type: String },
+        email: { 
+            type: String, 
+            required: true, 
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: { 
+            type: String, 
+            required: true 
+        },
+        username: { 
+            type: String, 
+            required: true, 
+            unique: true,
+            trim: true
+        },
+        firstName: { 
+            type: String, 
+            default: '' 
+        },
+        lastName: { 
+            type: String, 
+            default: '' 
+        },
         role: {
             type: String,
             enum: ['user', 'admin'],
             default: 'user',
+        },
+        profileImage: {  // Add this field
+            type: String,
+            default: ''
         }
     },
     {
@@ -21,6 +46,7 @@ const UserSchema: Schema = new Schema<UserType>(
 export interface IUser extends UserType, Document { 
     _id: mongoose.Types.ObjectId; 
     updatedAt: Date;
+    createdAt: Date;
 }
 
 export const UserModel = mongoose.model<IUser>('User', UserSchema);
